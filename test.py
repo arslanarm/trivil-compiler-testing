585058b71dd28cdb9807412926f82189cd51bc60
plony <esiredrise@gmail.com>
2023-11-13 21:46:09 +0300

    refactor project structure

--- test.py
+++ test.py
@@ -19,11 +19,13 @@ def main():
     parser.add_argument('inputdir', help='The input directory to test with "tric".')
     args = parser.parse_args()
 
-    for root, dirs, files in os.walk(args.inputdir):
-        for file in files:
-            result, output = test(os.path.join(root, file))
-            if not result:
-                print(f"Error in file {file}:\n{output}")
+    for root, dirs, files in os.walk(args.inputdir + "/positive"):
+        for dir in dirs:
+            for root_sub, dirs_sub, files_sub in os.walk(os.path.join(root, dir)):
+                for file in files_sub:
+                    result, output = test(os.path.join(root_sub, file))
+                    if not result:
+                        print(f"Error in file {file}:\n{output}")
  
 
 if __name__ == "__main__":
