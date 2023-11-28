e4cd9260f6adabf0f9e43b02de9adacf68991f75
plony <esiredrise@gmail.com>
2023-11-28 11:16:30 +0300

    added more tests

--- test.py
+++ test.py
@@ -9,24 +9,33 @@ def test(input_file):
     output = os.popen(f"tric {os.path.basename(input_file)}").read()
     
     os.remove(os.path.basename(input_file))
-    if "Без ошибок" in output:
-        os.remove('_'.join(os.path.basename(input_file).split('_')[1:])[:-3] + "exe")
-        return True, output
-    return False, output
+    if "Без ошибок" not in output:
+        return False, output
+    generated_file_name = '_'.join(os.path.basename(input_file).split('_')[1:])[:-3] + "exe"
+    status = os.system(f"./{generated_file_name}")
+    if status != 0:
+        return False, "Runtime error"
+    os.remove(generated_file_name)
+    return True, output
 
 def main():
     parser = argparse.ArgumentParser(description='CLI tool to test compiler "tric".')
     parser.add_argument('inputdir', help='The input directory to test with "tric".')
     args = parser.parse_args()
 
+    test_count = 0
+    fail_count = 0
+
     for root, dirs, files in os.walk(args.inputdir + "/positive"):
-        for dir in dirs:
-            for root_sub, dirs_sub, files_sub in os.walk(os.path.join(root, dir)):
-                for file in files_sub:
-                    result, output = test(os.path.join(root_sub, file))
-                    if not result:
-                        print(f"Error in file {file}:\n{output}")
- 
+        for file in files:
+            test_count += 1
+            result, output = test(os.path.join(root, file))
+            if not result:
+                fail_count += 1
+                print(f"Error in file {file}:\n{output}")
+    print(f"Successful tests: {test_count - fail_count}")
+    print(f"Overall test: {test_count}")
+
 
 if __name__ == "__main__":
     main()
